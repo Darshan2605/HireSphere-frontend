67f45dc9d0a93e6292caf0bce15c894dce9836ff
Darshan Londhe <116908065+Darshan2605@users.noreply.github.com>
2025-01-08 18:36:58 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,6 @@ const App = () => {
         <Route path="/applications/me" element={<MyApplications />} />
         <Route path="/job/post" element={<PostJob />} />
         <Route path="/job/me" element={<MyJobs />} />
-        {/* Remove JobOpenings route if component doesn't exist */}
          <Route path="/JobOpenings" element={<JobOpenings/>} /> 
         <Route path="*" element={<NotFound />} />
       </Routes>
